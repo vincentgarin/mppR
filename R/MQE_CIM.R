be05b6a6f5f5a51fa0f07d78a5ff6256264bef6d
Vincent Garin <vincent.garin@wur.nl>
2018-06-15 09:59:19 +0200

    remove function formPedMatInv and reference in functions

--- R/MQE_CIM.R
+++ R/MQE_CIM.R
@@ -96,10 +96,6 @@ MQE_CIM <- function(mppData = NULL, trait = 1, Q.eff = "cr", VCOV = "h.err",
   
   t_val <- sel_trait(mppData = mppData, trait = trait)
   
-  ### 2.2 inverse of the pedigree matrix
-  
-  # formPedMatInv(mppData = mppData, VCOV = VCOV)
-  
   ### 2.3 cross matrix (cross intercept)
   
   cross.mat <- IncMat_cross(cross.ind = mppData$cross.ind)
--- R/mpp_CIM_clu.R
+++ R/mpp_CIM_clu.R
@@ -25,10 +25,6 @@ mpp_CIM_clu <- function(mppData, trait = 1, Q.eff = "cr",
   
   t_val <- sel_trait(mppData = mppData, trait = trait)
   
-  ### 2.1 inverse of the pedigree matrix
-  
-  # formPedMatInv(mppData = mppData, VCOV = VCOV)
-  
   ### 2.2 cross matrix (cross intercept)
   
   cross.mat <- IncMat_cross(cross.ind = mppData$cross.ind)
